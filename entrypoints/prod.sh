984c095624ec2eded2f27b6ae5f9f392a3af97d1
Anar Guliyev <anar.guliyev@invitae.com>
2020-02-14 16:31:37 -0600

    for prod, set `SECRETS_FILE` to sample file
    it fails if file doesnt exist

--- entrypoints/prod.sh
+++ entrypoints/prod.sh
@@ -2,4 +2,4 @@
 
 # You can check ENTRYPOINT_MODE to run different stuff
 
-gunicorn --reload --log-level warning --log-file - --worker-class eventlet --workers 3 --timeout 300 --bind 0.0.0.0:5000 app.controller:app
+gunicorn --reload --log-level info --log-file - --worker-class eventlet --workers 3 --timeout 300 --bind 0.0.0.0:5000 app.controller:app
